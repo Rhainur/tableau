ee9fd25d6fdeec2d13dd21445c48d1d51b79a08d
Rohit Nair <admin@rohitnair.net>
2013-10-25 21:38:06 +0400

    View files

---
+++ app/views/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Tableau - Inspiration Browser</title>
+  <meta name="description" content="A web app to discover related images">
+  <meta name="author" content="Rohit Nair">
+
+  {{ HTML::style('css/style.css') }}
+</head>
+
+<body>
+  {{ HTML::script('//code.jquery.com/jquery-latest.min.js') }}
+  {{ HTML::script('js/script.js') }}
+</body>
+</html>
